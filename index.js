c7f0b6402ef7453497b9248599a0cea679432cdd
janmrakava <janmrakava@gmail.com>
2022-02-26 19:56:29 +0100

    Zaklad fungovani

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-
-
-
-
-
+let myLeads = []
+const inputEl = document.getElementById("input-el")
 let inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
 
 inputBtn.addEventListener("click", function() {
-  console.log("Button clicked from addEventListener");
+  myLeads.push(inputEl.value)
+  console.log(myLeads)
+  render()
 })
+
+function render(){
+  let listItems = ""
+for (let i = 0; i < myLeads.length; i++) {
+  //Create element, set text content, append to ul
+  listItems += "<li>" + myLeads[i] + "</li>"
+
+  //Nebo lze rozdělit do 3 nasledujiich prikazu
+  //const li = document.createElement("li")
+  //li.textContent = myLeads[i]
+  //ulEl.append(li)
+}
+  ulEl.innerHTML = listItems
+}
